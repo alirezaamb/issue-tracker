8b89392c518e3b0acc78f6cca7ddb4a999aa996c
alirezaamb <ababaei78@gmail.com>
2024-09-15 16:25:02 +0330

    feat:add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 import { ErrorMessage } from '@/app/components/ErrorMessage';
 import { createIssueSchema } from '@/app/ValidationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Spinner, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
@@ -16,6 +16,7 @@ type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
   const router = useRouter();
   const [error, setError] = useState('');
+  const [isSubmmiting, setIsSubmmiting] = useState(false);
   const {
     register,
     handleSubmit,
@@ -35,9 +36,12 @@ const NewIssuePage = () => {
         className=" space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIsSubmmiting(true);
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
+            setIsSubmmiting(false);
+
             setError('an unexpected error occured');
           }
         })}
@@ -52,7 +56,9 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button type="submit">Submit new Issue</Button>
+        <Button type="submit" disabled={isSubmmiting}>
+          Submit new Issue {isSubmmiting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
