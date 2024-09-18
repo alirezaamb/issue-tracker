fe4e3e94b09f646912384bbb6ae80bee5105a5ed
alirezaamb <ababaei78@gmail.com>
2024-09-18 09:36:55 +0330

    feat:add status changer in form and api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -22,6 +22,7 @@ export async function PATCH(
     data: {
       title: body.title,
       description: body.description,
+      status: body.status,
     },
   });
   return NextResponse.json(updateIssue);
